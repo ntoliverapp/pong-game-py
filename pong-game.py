e17e3ad30fba0bef3622d2dbf2a2990d9fa1928d
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-14 04:48:58 -0700

    fixed some bugs

--- pong-game.py
+++ pong-game.py
@@ -1,118 +1,136 @@
-import turtle as turtleplayerAscore
+import turtle as t
+
+
 player_one_score=0
 player_two_score=0
 
 window=t.Screen()
-window.title("Pong Game")
-window.bgcolor("red")
+window.title('Ping-Pong Game')
+window.bgcolor('#162521')
 window.setup(width=800,height=600)
 window.tracer(0)
 
 #creating left paddle
-leftpaddle=t.Turtle()
-leftpaddle.speed(0)
-leftpaddle.shape('square')
-leftpaddle.color('white')
-leftpaddle.shapesize(stretch_wid=5,stretch_len=1)
-leftpaddle.penup()
-leftpaddle.goto(-350,0)
+left_paddle=t.Turtle()
+left_paddle.speed(0)
+left_paddle.shape('square')
+left_paddle.color('#c0e0de')
+left_paddle.shapesize(stretch_wid=5,stretch_len=1)
+left_paddle.penup()
+left_paddle.goto(-350,0)
+
 
 #creating right paddle
-rightpaddle=t.Turtle()
-rightpaddle.speed(0)
-rightpaddle.shape('square')
-rightpaddle.color('white')
-rightpaddle.shapesize(stretch_wid=5,stretch_len=1)
-rightpaddle.penup()
-rightpaddle.goto(350,0)
+right_paddle=t.Turtle()
+right_paddle.speed(0)
+right_paddle.shape('square')
+right_paddle.color('#c0e0de')
+right_paddle.shapesize(stretch_wid=5,stretch_len=1)
+right_paddle.penup()
+right_paddle.goto(350,0)
 
 #creating ball
-ball=t.Turtle
+ball=t.Turtle()
 ball.speed(0)
 ball.shape('circle')
 ball.color('red')
 ball.penup()
-ball.goto(5,5)
-ballxdirection=0.2
-ballydirection=0.2
+ball.goto(0,0)
+ball_x_direction=1.5
+ball_y_direction=1.5
 
 #creating pen for scorecard update
-pen=t.Turtle
+pen = t.Turtle()
 pen.speed(0)
-pen.color('Blue')
+pen.color('white')
 pen.penup()
 pen.hideturtle()
-pen,goto(0,260)
-pen.write('score',align='center', font=('Verdana', 28,'normal'))
+pen.goto(0,260)
+pen.write('Score',align='center', font=('comic sans MS', 20,'normal'))
+
+
 
 #moving the leftpaddle
-def leftpaddleup():
-    y=leftpaddle.ycor()
-    y=y+90
-    leftpaddle.sety(y)
+def left_paddle_up():
+    y = left_paddle.ycor()
+    y = y + 15
+    left_paddle.sety(y)
     
-def leftpaddledown():
-    y=leftpaddle.ycor()
-    y=y-90
-    leftpaddle.sety(y)
+def left_paddle_down():
+    y = left_paddle.ycor()
+    y = y - 15
+    left_paddle.sety(y)
     
 #moving the right paddle
-def rightpaddleup():
-    y=rightpaddle.ycor()
-    y=y+90
-    rightpaddle.sety(y)
+def right_paddle_up():
+    y = right_paddle.ycor()
+    y = y + 15
+    right_paddle.sety(y)
     
-def rightpaddledown():
-    y=rightpaddle.ycor()
-    y=y-90
-    rightpaddle.sety(y)
+def right_paddle_down():
+    y = right_paddle.ycor()
+    y = y - 15
+    right_paddle.sety(y)
     
 #Assign keys to play
 window.listen()
-window.onkeypress(leftpaddleup,'q')
-window.onkeypress(leftpaddledown,'a')
-window.onkeypress(rightpaddleup, 'Up')
-window.onkeypress(rightpaddledown, 'Down')
+window.onkeypress(left_paddle_up,'a')
+window.onkeypress(left_paddle_down,'z')
+window.onkeypress(right_paddle_up, 'Up')
+window.onkeypress(right_paddle_down, 'Down')
 
 while True:
     window.update()
+    
 
 #moving the ball
-ball.setx(ball.xcor()+ballxdirection)
-ball.sety(ball.ycor()+ballydirection)
+    ball.setx(ball.xcor() + ball_x_direction)
+    ball.sety(ball.ycor() + ball_y_direction)
 
 #setting up the border
-if ball.ycor()>290:
-    ball.sety(290)
-    ballydirection=ballydirection*-1
-if ball.ycor()>-290:
-    ball.sety(-290)
-    ballydirection=ballydirection*-1    
-
-if ball.xcor()>390:
-    ball.goto(0,0)
-    ballxdirection=ballxdirection
-    player_one_score=player_one_score+1
-    pen.clear
-    pen.write('Player 1: {}      Player 2: {}'.format(player_one_score,player_two_score),align='center',font=('Verdana', 28,'normal'))
+    if ball.ycor() > 290: #right top paddle border
+        ball.sety(290)
+        ball_y_direction = ball_y_direction * -1
     
-if ball.xcor()>-390:
-    ball.goto(0,0)
-    ballxdirection=ballxdirection*-1
-    player_one_score=player_one_score+1
-    pen.clear
-    pen.write('Player 1: {}      Player 2: {}'.format(player_one_score,player_two_score),align='center',font=('Verdana', 28,'normal'))
+    if ball.ycor()< -290: #left top paddle border
+        ball.sety(-290)
+        ball_y_direction = ball_y_direction * -1    
+        
+
+    if ball.xcor() > 390: #right width paddle border
+        ball.goto(0,0)
+        ball_x_direction = ball_x_direction * -1
+        player_one_score = player_one_score + 1
+        pen.clear
+        pen.write('Player 1: {}      Player 2: {}'.format(player_one_score,player_two_score),align='center',font=('comic sans MS', 20,'normal'))
+  
+    
+    if ball.xcor() < -390: #left width paddle border
+        ball.goto(0,0)
+        ball_x_direction = ball_x_direction * -1
+        player_one_score = player_one_score + 1
+        pen.clear()
+        pen.write('Player 1: {}      Player 2: {}'.format(player_one_score,player_two_score),align='center',font=('comic sans MS', 20,'normal'))
+
     
 #Handling the Collisions
-if(ball.xcor()>340)and(ball.xcor()<350)and(
-    ball.ycor()<rightpaddle.ycor()+40 and ball.ycor()>rightpaddle.ycor()-40):
-    ball.setx(340)
-    ballxdirection=ballxdirection*-1
+    if(ball.xcor() > 340) and (ball.xcor() < 350) and (ball.ycor() < right_paddle.ycor() + 40 and ball.ycor() > right_paddle.ycor() -40):
+        ball.setx(340)
+        ball_x_direction = ball_x_direction * -1
 
-if(ball.xcor()<-340)and(ball.xcor()>-350)and(
-    ball.ycor()<leftpaddle.ycor()+40 and ball.ycor()>leftpaddle.ycor()-40):
-    ball.setx(-340)
-    ballxdirection=ballxdirection*-1
+    if(ball.xcor() < -340) and (ball.xcor() > -350) and (ball.ycor() < left_paddle.ycor() +40 and ball.ycor() > left_paddle.ycor() -40):
+        ball.setx(-340)
+        ball_x_direction = ball_x_direction * -1
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
    
 
